61f971d92b2e065883437907b130c27aa4a227db
Ravshan Mahmudov <2321772@gmail.com>
2022-01-23 19:19:24 +0200

    js files

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,24 +1,33 @@
-const links = document.querySelectorAll('.header-menu__item a');
+const scrollFunc = () => {
+  const links = document.querySelectorAll(".header-menu__item a");
+  const linkCharacteristics = document.querySelector(
+    ".card-details__link-characteristics"
+  );
 
-seamless.polyfill();
+  const newArray = [...links, linkCharacteristics];
+  console.log(newArray);
 
+  seamless.polyfill();
 
-links.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault();
-    const id = element.getAttribute('href').substring(1);
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({
-        behavior: 'smooth',
-        block:'start'
-      });
-    } else {
+  newArray.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
+      const id = element.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
+      if (section) {
+        section.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      } else {
         seamless.elementScrollIntoView(document.querySelector("#target"), {
-        behavior: "smooth",
-        block: "center",
-        inline: "center",
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        });
+      }
     });
-    }
-  })
-})
+  });
+};
+
+scrollFunc();
---
+++ scripts/tabs.js
@@ -0,0 +1,62 @@
+const tabsFunc = () => {
+  const tabs = document.querySelectorAll(".card-detail__change");
+  const tabsTitle = document.querySelector(".card-details__title");
+  const tabsPrice = document.querySelector(".card-details__price");
+  const tabsMemory = document.querySelectorAll(
+    ".card-details__description-item description__memory"
+  );
+
+  const tabsImage = document.querySelector(".card__image_item");
+
+  const tabsOptions = [
+    {
+      name: "Graphite",
+      memory: "128",
+      cost: 65000,
+      image: "img/iPhone-graphite.webp",
+    },
+    {
+      name: "Silver",
+      memory: "256",
+      cost: 79000,
+      image: "img/iPhone-silver.webp",
+    },
+    {
+      name: "Sierra Blue",
+      memory: "512",
+      cost: 99000,
+      image: "img/iPhone-sierra_blue.webp",
+    },
+  ];
+
+  const changeContent = (index) => {
+    tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
+    tabsMemory.textContent = `Встроенная память (ROM) ${tabsOptions[index].memory} ГБ`;
+    tabsPrice.textContent = `${tabsOptions[index].cost}₽`;
+
+    tabsImage.setAttribute("src", tabsOptions[index].image);
+  };
+
+  const changeActiveTabs = (indexClickedTab) => {
+    tabs.forEach((tab, index) => {
+      tab.classList.remove("active");
+
+      if (index === indexClickedTab) {
+        tab.classList.add("active");
+      }
+    });
+    changeContent(indexClickedTab);
+  };
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      changeActiveTabs(index);
+    });
+  });
+
+  changeContent(0);
+
+  // console.log(tabs);
+};
+
+tabsFunc();
